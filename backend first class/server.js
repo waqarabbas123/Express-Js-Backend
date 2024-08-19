d51a691546a5d3c9080f187bfc812d2b16443edd
B E R L I N <waqarabbas123>
2024-08-19 13:02:37 +0500

    backend classes

---
+++ backend first class/server.js
@@ -0,0 +1,6 @@
+import {a , b ,d}  from "./hello.js"  //import base on named method
+import obj from "./hello.js"  //import based on defult method
+ console.log(a , b  ,d );
+ 
+ console.log(obj.x);
+ 
